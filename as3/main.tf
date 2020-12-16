00fda2944f875c6660cec8dad32fd8675fce2cbb
Sanjay <scshitole@gmail.com>
2020-12-16 11:42:13 -0800

    updated nginx json

--- as3/main.tf
+++ as3/main.tf
@@ -16,6 +16,5 @@ terraform {
 # deploy application using as3
 resource "bigip_as3" "nginx" {
   as3_json    = file("nginx.json")
-  tenant_name = "consul_sd"
 #  depends_on  = [null_resource.install_as3]
 }
